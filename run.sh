fa38b7ccaceb5caa92d84307d6eb724281618557
exaithrg <syhaoran@163.com>
2025-02-05 10:01:21 +0800

    add __pycache__ to gitignore

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# ./llama3 ./models/Llama3.1-8B-q80.bin -i "<HERE GOES THE PROMPT>"
-
-./build/llama3 ./models/Llama3.2-1B.bin -i "Llama is an accessible, open large language model (LLM) designed for"
